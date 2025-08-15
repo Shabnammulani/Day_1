596cc0371f6c93d885c4f76afa7b2de74b37b18a
Shabnammulani <115937552+Shabnammulani@users.noreply.github.com>
2025-08-15 21:05:37 +0530

    Add files via upload

---
+++ DemoClass.java
@@ -0,0 +1,14 @@
+package Day_1;
+
+class DemoClass{
+
+	public static void main(String[] args){
+
+		System.out.println("Hello New Learners!!!,Welcome");
+
+	}
+
+}
+
+
+
---
+++ DemoClasssProgram.java
@@ -0,0 +1,35 @@
+package Day_1;
+
+public class DemoClasssProgram {
+
+	
+
+		public static void main(String[] args){
+
+			System.out.println("Hello New Learners!!!,Welcome");
+
+			args[0]="234";
+
+			args[1]="34637";
+
+			System.out.println("args[0]: "+args[0]);
+
+			System.out.println("args[1]: "+args[1]);
+
+			System.out.println("Addition: "+(args[0]+args[1]));
+
+
+
+			int num1 = Integer.parseInt(args[0]);
+
+			int num2 = Integer.parseInt(args[1]);
+
+			System.out.println("New Addition: "+(num1+num2));
+
+
+
+		}
+
+	}
+	 
+	
